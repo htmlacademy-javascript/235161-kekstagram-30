e32f6078100805c09b4cb8fee9608f538f8502d5
Quistis <suicidexhelia@gmail.com>
2023-10-11 21:51:30 +0500

    Добавляет функции для создания массива из сгенерированных объектов

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// Моковые данные для заполнения объектов
-
 const COMMENT_MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -18,10 +16,33 @@ const AUTHOR_NAMES = [
   'Никс',
   'Загрей',
   'Аид',
-  'Ахилес'
+  'Ахиллес',
+  'Арес',
+  'Дионис'
 ];
 
-//Функция для генерации случайного целого числа
+const DESCRIPTIONS = [
+  'Снимок заката на океане, где небо окрашено в яркие оттенки оранжевого и розового.',
+  'Портрет счастливой семьи, гуляющей в парке, улучшающей летний день.',
+  'Фотография пышных гор на фоне ясного голубого неба.',
+  'Макро снимок капель дождя на листьях, отражающих мир в своих каплях.',
+  'Архитектурный кадр с изогнутым мостом в городском парке.',
+  'Морская панорама с величественными волнами и маяком на горизонте.',
+  'Пейзаж с горным хребтом, покрытым снегом, и заснеженной деревней внизу.',
+  'Мистический лес с солнечными лучами, проникающими сквозь деревья.',
+  'Фото цветущего вишневого дерева в весеннем саду.',
+  'Портрет собаки, счастливо играющей на пляже.',
+  'Городской пейзаж с ночными огнями и множеством автомобилей.',
+  'Макро снимок падающих капель дождя на асфальте.',
+  'Портрет молодой художницы, окруженной своими произведениями.',
+  'Абстрактное искусство с яркими красками и геометрическими фигурами.',
+  'Пейзаж с золотистыми полями и старой ветряной мельницей.',
+  'Фотография старинного книжного магазина с полками, набитыми книгами.',
+  'Архитектурный снимок древнего замка с мостом через ров.',
+  'Снимок городской улицы в дождливый день, отражающейся в мокрых асфальтовых дорогах.',
+  'Портрет молодой балерины в костюме, выполняющей изящные движения.',
+  'Морской закат с игрой отражений на воде и мягкими пастельными оттенками.'
+];
 
 const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
@@ -30,21 +51,63 @@ const getRandomPositiveInteger = (a, b) => {
   return Math.floor(result);
 };
 
-const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+const createRandomIdFromRangeGenerator = (min, max) => {
+  const generatedValues = [];
 
+  return () => {
 
-// Функия для создания объектов
-const createObject = () => {
-  return {
-    id: '',
-    url: '',
-    description: '',
-    likes: '',
-    comment: {
-      id: '',
-      avatar: '',
-      message: '',
-      name: ''
+    let currentGeneratedValue = getRandomPositiveInteger(min, max);
+
+    if (generatedValues.length >= (max - min + 1)) {
+      return null;
     }
+
+    while (generatedValues.includes(currentGeneratedValue)) {
+      currentGeneratedValue = getRandomPositiveInteger(min, max);
+    }
+
+    generatedValues.push(currentGeneratedValue);
+    return currentGeneratedValue;
   };
 };
+
+const generatePhotoId = createRandomIdFromRangeGenerator(2, 25);
+const generateImgUrlId = createRandomIdFromRangeGenerator(1, 25);
+const generateCommentId = createRandomIdFromRangeGenerator(1, 1000);
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+const generateCommentMessage = () => {
+  let commentMessage = '';
+  const generatedMessagePart = getRandomArrayElement(COMMENT_MESSAGES);
+  let anotherGeneratedMessagePart = getRandomArrayElement(COMMENT_MESSAGES);
+  const messageParts = getRandomPositiveInteger(1, 2);
+
+  if (messageParts > 1) {
+    while (generatedMessagePart === anotherGeneratedMessagePart) {
+      anotherGeneratedMessagePart = getRandomArrayElement(COMMENT_MESSAGES);
+    }
+    commentMessage += generatedMessagePart + anotherGeneratedMessagePart;
+    return commentMessage;
+  }
+
+  commentMessage += generatedMessagePart;
+  return commentMessage;
+};
+
+const generateComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${ getRandomPositiveInteger(1, 6) }.svg`,
+  message: generateCommentMessage(),
+  name: getRandomArrayElement(AUTHOR_NAMES)
+});
+
+const createPhotoDescription = () => ({
+  id: generatePhotoId(),
+  url: `photos/${ generateImgUrlId() }.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomPositiveInteger(15, 200),
+  comment: Array.from({length: getRandomPositiveInteger(0, 30)}, generateComment)
+});
+
+const getPhotoDescriptions = Array.from({length: 20}, createPhotoDescription);
+export {getPhotoDescriptions};
