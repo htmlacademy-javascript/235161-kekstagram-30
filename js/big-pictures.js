25f205562baca5b0eca4264c68fffc7082605e44
Quistis <suicidexhelia@gmail.com>
2023-10-21 22:15:12 +0500

    Добавляет функцию createComment для генерации и добавления комментариев.

--- js/big-pictures.js
+++ js/big-pictures.js
@@ -2,14 +2,37 @@ import { isEscapeKey } from './util.js';
 
 const bigPictureContainer = document.querySelector('.big-picture');
 const bigPictureCloseButton = document.querySelector('.big-picture__cancel');
+const commentsList = document.querySelector('.social__comments');
+commentsList.innerHTML = '';
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     bigPictureContainer.classList.add('hidden');
     document.body.classList.remove('modal-open');
+    commentsList.innerHTML = '';
     //closePicture();
   }
+};
+
+const createComment = (comment) => {
+  const commentElement = document.createElement('li');
+  commentElement.classList.add('social__comment');
+
+  const commentAvatar = document.createElement('img');
+  commentAvatar.classList.add('social__picture');
+  commentAvatar.src = comment.avatar;
+  commentAvatar.alt = comment.name;
+  commentAvatar.width = 35;
+  commentAvatar.height = 35;
+
+  const commentMessage = document.createElement('p');
+  commentMessage.classList.add('social__text');
+  commentMessage.textContent = comment.message;
+
+  commentElement.append(commentAvatar, commentMessage);
+
+  return commentElement;
 };
 
 const openPicture = (item) => {
@@ -17,6 +40,11 @@ const openPicture = (item) => {
   bigPictureContainer.querySelector('.likes-count').textContent = item.likes;
   bigPictureContainer.querySelector('.social__comment-total-count').textContent = item.comments.length;
   bigPictureContainer.querySelector('.social__caption').textContent = item.description;
+
+  const comments = item.comments;
+  comments.forEach((comment) => {
+    commentsList.append(createComment(comment));
+  });
 
   bigPictureContainer.classList.remove('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
@@ -29,6 +57,7 @@ const openPicture = (item) => {
 const closePicture = () => {
   bigPictureContainer.classList.add('hidden');
   document.body.classList.remove('modal-open');
+  commentsList.innerHTML = '';
 
   document.removeEventListener('keydown', onDocumentKeydown);
 };
