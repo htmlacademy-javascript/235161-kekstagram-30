1bb3bebac47cb4b5fc87d64177d3f8ad2e2b6612
Quistis <suicidexhelia@gmail.com>
2023-10-25 23:39:15 +0500

    Добавляет валидацию хэштегов. Пока что не готова проверка на уникальность

--- js/image-upload-form.js
+++ js/image-upload-form.js
@@ -3,6 +3,7 @@ import { isEscapeKey } from './util';
 const imgUploadForm = document.querySelector('.img-upload__form');
 const imgUploadInput = imgUploadForm.querySelector('.img-upload__input');
 const imgEditForm = document.querySelector('.img-upload__overlay');
+const imgEditHashtagsInput = document.querySelector('.text__hashtags');
 const imgEditCloseButton = document.querySelector('.img-upload__cancel');
 
 const closeImgEditModal = () => {
@@ -10,6 +11,8 @@ const closeImgEditModal = () => {
   document.body.classList.remove('modal-open');
 
   imgUploadInput.value = '';
+  imgEditHashtagsInput.value = '';
+  //Здесь нужно будет добавить сброс остальных полей формы, но это потом
 };
 
 const onImgEditCloseButtonClick = () => {
@@ -42,17 +45,42 @@ const onImgUploadButtonClick = () => {
 
 imgUploadInput.addEventListener('change', onImgUploadButtonClick);
 
-//Тут будет валидация
+//Регулярное выражение сделанное по примеру из видео
+const regexp = /^#[a-zа-яё0-9]{1,19}$/i;
+
+//Тут будет валидация. Почти сделал валидацию хэштегов, осталась проверка на уникальность
 const pristine = new Pristine(imgUploadForm);
 
-imgUploadForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
+const validateHashtags = (hashtags) => {
+  const splitHashtags = hashtags.trim().split(' ');
 
+  if (splitHashtags.length > 5) {
+    return false;
+  }
+
+  for (let i = 0; i < splitHashtags.length; i++) {
+    //Кривая проверка на то, что инпут с хэштэгами пуст, не смог решить почему если отправлять пустой инпут в массив попадает элемент = ''
+    if (splitHashtags[i] === '' && splitHashtags.length === 1) {
+      return true;
+    }
+
+    if (!regexp.test(splitHashtags[i])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+pristine.addValidator(imgEditHashtagsInput, validateHashtags);
+
+imgUploadForm.addEventListener('submit', (evt) => {
   const isValid = pristine.validate();
 
   if(isValid) {
     //console.log('Можно отправлять');
   } else {
     //console.log('Форма невалидна');
+    evt.preventDefault();
   }
 });
