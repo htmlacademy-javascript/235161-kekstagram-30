eccf847c23f650f89e70882b27ff1265c476840d
Keks <keks@htmlacademy.ru>
2023-11-11 16:25:36 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/renderPictures.js
@@ -0,0 +1,46 @@
+import {openPicture} from './big-pictures.js';
+
+const picturesContainer = document.querySelector('.pictures');
+
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const picturesFragment = document.createDocumentFragment();
+
+const clearPictures = () => {
+  const picturesOnPage = document.querySelectorAll('.picture');
+  picturesOnPage.forEach((picture) => picture.remove());
+};
+
+const renderPicture = (photo) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = photo.url;
+  pictureElement.querySelector('.picture__img').alt = photo.description;
+  pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+
+  return pictureElement;
+};
+
+const renderPictures = (photos) => {
+
+  photos.forEach((item) => {
+    const thumbnail = renderPicture(item);
+    picturesFragment.append(thumbnail);
+
+    const onThumbnailClick = (evt) => {
+      evt.preventDefault();
+
+      openPicture(item);
+    };
+
+    thumbnail.addEventListener('click', onThumbnailClick);
+  });
+
+  clearPictures();
+  picturesContainer.append(picturesFragment);
+};
+
+export {renderPictures};
